fa99dd76ba5acd7556ffb3454b2c686bacf1ffb4
IvashchenkoIvanIgorevich <ivachko2009@gmail.com>
2021-07-25 12:47:54 +0300

    Create new .gitignore file

---
+++ DAL/Models/Filial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Models
+{
+    public partial class Filial
+    {
+        public Filial()
+        {
+            Library = new HashSet<Library>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public virtual ICollection<Library> Library { get; set; }
+    }
+}
---
+++ DAL/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DAL.Models
+{
+    public partial class Order
+    {
+        public int Id { get; set; }
+        public DateTime TakeDay { get; set; }
+        public DateTime? BackDay { get; set; }
+        public int? IdBook { get; set; }
+        public int? IdUser { get; set; }
+
+        public virtual Book IdBookNavigation { get; set; }
+        public virtual User IdUserNavigation { get; set; }
+    }
+}
