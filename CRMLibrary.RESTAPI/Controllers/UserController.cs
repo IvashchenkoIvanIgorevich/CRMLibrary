c6cf9304d6e0063e080b3f3fc329809b6315d46a
IvashchenkoIvanIgorevich <ivachko2009@gmail.com>
2021-09-06 19:01:15 +0300

    Solved problem with css and html errors in console.

---
+++ CRMLibrary.RESTAPI/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CRMLibrary.RESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        LibraryContext db = new LibraryContext();
+
+        [HttpGet]        
+        public ActionResult<IEnumerable<User>> Index() => db.User.ToList();
+    }
+}
